01edfa0c3b6b31ec27b5ac22cb5236a8dd58d37f
shin <xxin11873@gmail.com>
2026-08-09 12:22:00 +0900

    TestScreen,ProfileScreen,PostScreenの作成

---
+++ src/screens/TestScreen.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+
+export function TestScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Test</Text>
+    //componentsのテスト
+    </View>
+  );
+}
