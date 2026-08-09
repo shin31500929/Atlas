84640bdcfc005721eb39a749be260ff6806b3a09
shin <xxin11873@gmail.com>
2026-08-09 23:43:29 +0900

    Buttonの追加

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -16,6 +16,10 @@ export function HomeScreen({ navigation }: Props) {
       <Text>Atlas</Text>
       <Button title="Go to Post" onPress={() => navigation.navigate("Post")} />
       <Button title="Go to Profile" onPress={() => navigation.navigate("Profile")} />
+      <Button title="Go to Login" onPress={() => navigation.navigate("Login")} />
+      <Button title="Go to Recording" onPress={() => navigation.navigate("Recording")} />
+      <Button title="Go to Confirm" onPress={() => navigation.navigate("Confirm")} />
+      <Button title="Go to RecordingMap" onPress={() => navigation.navigate("RecordingMap")} />
       <Button title="Go to Test" onPress={() => navigation.navigate("Test")} />
     </View>
   );
