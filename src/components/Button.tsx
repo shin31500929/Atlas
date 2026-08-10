03e3127e7689baefff712b540f3bc6c1a5cfc196
suzuran18067 <hrk.ktn@icloud.com>
2026-08-10 16:19:31 +0900

    Revert "button.tsxの作成"

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,53 +1,10 @@
-import * as React from "react";
-import { View } from "react-native";
-import { Button } from "react-native-paper";
-import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import * as React from 'react';
+import { Button } from 'react-native-paper';
 
-type ButtonComponentProps = {
-  text: string;
-  iconName: React.ComponentProps<typeof MaterialCommunityIcons>["name"];
-  onPress: () => void;
-  buttonColor: string;
-  textColor: string;
-  borderColor?: string;
-};
-
-const ButtonComponent = ({
-  text,
-  iconName,
-  onPress,
-  buttonColor,
-  textColor,
-  borderColor
-}: ButtonComponentProps) => (
-  <View style={{ flexDirection: "row", gap: 10 }}>
-    <Button
-      icon={({color}) => (
-        <MaterialCommunityIcons
-            name={iconName}
-            size={25}
-            color={color} 
-        />
-      )}
-      mode="contained"
-      buttonColor={buttonColor}
-      textColor={textColor}
-      contentStyle={{
-        alignItems: "center",
-      }}
-      style={{
-        ...(borderColor 
-          ? {
-            borderColor,
-            borderWidth: 1 
-          } 
-        : {}),
-      }}
-      onPress={onPress}
-    >
-      {text}
-    </Button>
-  </View>
+const ButtonComponent = () => (
+  <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+    Press me
+  </Button>
 );
 
 export default ButtonComponent;
