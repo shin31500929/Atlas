d270b39a790e843f3fd0968447b27ecdcc466b6a
suzuran18067 <hrk.ktn@icloud.com>
2026-08-25 02:13:21 +0900

    Input.tsxの修正

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,52 +1,51 @@
 import { TextInput } from "react-native-paper";
-import { StyleProp, ViewStyle } from "react-native";
+import {StyleProp,TextStyle,ViewStyle,} from "react-native";
 
 type InputProps = {
-    value: string;
-    onChangeText: (text: string) => void;
+  value: string;
+  onChangeText: (text: string) => void;
 
-    activeOutlineColor?: string;
-    outlineColor?: string;
-    textColor?: string;
+  activeOutlineColor?: string;
+  outlineColor?: string;
+  textColor?: string;
 
-    placeholder?: string;
-    placeholderTextColor?: string;
-    backgroundColor?: string;
+  placeholder?: string;
+  placeholderTextColor?: string;
+  backgroundColor?: string;
 
-    style?: StyleProp<ViewStyle>;
-    outlineStyle?: StyleProp<ViewStyle>;
-
+  style?: StyleProp<TextStyle | ViewStyle>;
+  outlineStyle?: StyleProp<ViewStyle>;
 };
 
 const Input = ({
-    value,
-    onChangeText,
-    activeOutlineColor,
-    outlineColor,
-    textColor,
-    placeholder,
-    placeholderTextColor,
-    backgroundColor,
-    style,
-    outlineStyle,
+  value,
+  onChangeText,
+  activeOutlineColor,
+  outlineColor,
+  textColor,
+  placeholder,
+  placeholderTextColor,
+  backgroundColor,
+  style,
+  outlineStyle,
 }: InputProps) => (
-    <TextInput
-        mode="outlined"
-        value={value}
-        onChangeText={onChangeText}
-        textColor={textColor}
-        placeholder={placeholder}
-        placeholderTextColor={placeholderTextColor}
-        activeOutlineColor={activeOutlineColor}
-        outlineColor={outlineColor}
-        style={[
-            {
-                backgroundColor: backgroundColor,
-            },
-            style,
-        ]}
-        outlineStyle={outlineStyle}
-    />
+  <TextInput
+    mode="outlined"
+    value={value}
+    onChangeText={onChangeText}
+    textColor={textColor}
+    placeholder={placeholder}
+    placeholderTextColor={placeholderTextColor}
+    activeOutlineColor={activeOutlineColor}
+    outlineColor={outlineColor}
+    style={[
+      {
+        backgroundColor: backgroundColor,
+      },
+      style,
+    ]}
+    outlineStyle={outlineStyle}
+  />
 );
 
 export default Input;
--- frontend/src/screens/TestScreen.tsx
+++ frontend/src/screens/TestScreen.tsx
@@ -17,21 +17,21 @@ function TestScreen() {
       {/*componentsのテスト*/}
 
       <Input
-        placeholder="文字を入力"
-        value={name}
-        onChangeText={setName}
-        activeOutlineColor="#0000ff"
-        outlineColor="#ff0000"
-        textColor="#283D36"
-        placeholderTextColor="#888888"
-        backgroundColor="#f9f9f9"
+        placeholder="文字を入力"  //入力欄の中の文字を指定
+        value={name}  //入力欄に現在表示する文字列を指定
+        onChangeText={setName}  //入力された文字が変化したときに呼び出す関数を指定
+        activeOutlineColor="#6b7974"  //入力欄がアクティブになった時の枠線の色を指定
+        outlineColor="#a8bab2"  //入力欄がアクティブでない時の枠線の色を指定
+        textColor="#283D36" //入力欄の中の文字の色を指定
+        placeholderTextColor="#888888"  //入力欄の中の文字がない時の文字の色を指定
+        backgroundColor="#f9f9f9" //入力欄の背景色を指定
         style={{
           width: 300,
           height: 50,
-        }}
+        }}//入力欄の大きさを指定
         outlineStyle={{
           borderRadius: 25,
-        }}
+        }}//入力欄の枠線の角の丸みを指定
       />
     </View>
   );
