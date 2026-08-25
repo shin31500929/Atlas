baca0f5867343192b023ec1d57c8c5958233e57f
suzuran18067 <hrk.ktn@icloud.com>
2026-08-25 14:50:45 +0900

    flexの削除

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -18,7 +18,6 @@ const Loading = ({ color, size }: LoadingProps) => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
