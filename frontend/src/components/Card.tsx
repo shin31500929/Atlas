b71d5c42a7505bd09c141d594b7908f0206b7b4a
Seiya <seiya314159265@icloud.com>
2026-08-27 05:43:25 +0900

    feat:何時間前表示を実装

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -2,20 +2,30 @@ import { ImageSourcePropType } from "react-native";
 import { Avatar, Button, Card, Text } from "react-native-paper";
 
 function CalcDiffTime(PostedAt: Date) {
+  const options = {
+    weekday: "short" as const,
+    year: "numeric" as const,
+    month: "long" as const,
+    day: "numeric" as const,
+  };
   const now = new Date();
   const CurrentTime = now.getTime();
   const PostedAtTime = PostedAt.getTime();
-  let PassedHour = 0;
-  let diff = (CurrentTime - PostedAtTime) / (1000 * 60 * 60);
-  let diff_mm = ()
+  let PassedTime: string = "";
+  let diff_hh = Math.floor((CurrentTime - PostedAtTime) / (1000 * 60 * 60));
+  let diff_mm = Math.floor((CurrentTime - PostedAtTime) / (1000 * 60));
 
-  diff > 24 ? (PassedHour = PostedAtTime) : (PassedHour = diff);
-  return PassedHour;
+  diff_hh > 24
+    ? (PassedTime = `${PostedAt.toLocaleString("ja-JP", options)}`)
+    : 24 > diff_hh && diff_hh >= 1
+      ? (PassedTime = `${diff_hh.toLocaleString()}時間前`)
+      : (PassedTime = `${diff_mm.toLocaleString()}分前`);
+  return PassedTime;
 }
 
 type Props = {
   UserId: string;
-  PostedAt: Date;
+  PostedAt?: Date;
   Image?: ImageSourcePropType;
   Text?: string;
   SubTitle?: string;
@@ -26,7 +36,7 @@ const CardComponent = (props: Props) => (
     <Card.Content>
       <Card.Title
         title={props.UserId}
-        subtitle={CalcDiffTime(props.PostedAt)}
+        subtitle={props.PostedAt ? CalcDiffTime(props.PostedAt) : null}
         left={(ImageProps) =>
           props.Image ? (
             <Avatar.Image
