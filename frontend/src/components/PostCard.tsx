e14a331f4c53ad1976d63ed67a0587e93c1148d6
Seiya <seiya314159265@icloud.com>
2026-08-29 15:17:31 +0900

    Card.tsxをPostCard.tsxの方へ移動

---
+++ frontend/src/components/PostCard.tsx
@@ -0,0 +1,70 @@
+import { ImageSourcePropType } from "react-native";
+import { Avatar, Button, Card } from "react-native-paper";
+
+function CalcDiffTime(PostedAt: Date) {
+  const options = {
+    weekday: "short" as const,
+    year: "numeric" as const,
+    month: "long" as const,
+    day: "numeric" as const,
+  };
+  const now = new Date();
+  const CurrentTime = now.getTime();
+  const PostedAtTime = PostedAt.getTime();
+  let PassedTime: string = "";
+  let diff_hh = Math.floor((CurrentTime - PostedAtTime) / (1000 * 60 * 60));
+  let diff_mm = Math.floor((CurrentTime - PostedAtTime) / (1000 * 60));
+
+  if (diff_hh >= 24) {
+    PassedTime = `${PostedAt.toLocaleString("ja-JP", options)}`;
+  } else if (diff_hh >= 1) {
+    PassedTime = `${diff_hh.toLocaleString()}時間前`;
+  } else {
+    PassedTime = `${diff_mm.toLocaleString()}分前`;
+  }
+  return PassedTime;
+}
+
+const handleClick = (option: string) => {
+  console.log(`${option}がクリックされました`);
+};
+
+type Props = {
+  userId: string; // ユーザID
+  postedAt: Date; //　投稿日時
+  iconImage?: ImageSourcePropType; // アイコン
+  postedImage: ImageSourcePropType; // 投稿画像
+  // title: string; // タイトル
+  // text?: string;
+};
+
+const CardComponent = (props: Props) => (
+  <Card>
+    <Card.Content>
+      <Card.Title
+        title={props.userId}
+        subtitle={props.postedAt ? CalcDiffTime(props.postedAt) : null}
+        left={(ImageProps) => {
+          if (props.iconImage) {
+            return <Avatar.Image {...ImageProps} source={props.iconImage} />;
+          } else {
+            return (
+              <Avatar.Image
+                {...ImageProps}
+                source={{ uri: "https://picsum.photos/700" }}
+              />
+            );
+          }
+        }}
+      />
+      {/* <Text variant="bodyMedium">{props.text}</Text> */}
+    </Card.Content>
+    <Card.Cover source={props.postedImage} />
+    <Card.Actions>
+      <Button onPress={() => handleClick("Cancel")}>Cancel</Button>
+      <Button onPress={() => handleClick("Ok")}>Ok</Button>
+    </Card.Actions>
+  </Card>
+);
+
+export default CardComponent;
