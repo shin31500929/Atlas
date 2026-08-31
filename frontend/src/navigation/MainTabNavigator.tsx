a834124c48ec5e33b9e1d15864c47827142f8eb7
Seiya <seiya314159265@icloud.com>
2026-08-31 16:11:26 +0900

    feat:なんか動くようになったので一旦コミット

--- frontend/src/navigation/MainTabNavigator.tsx
+++ frontend/src/navigation/MainTabNavigator.tsx
@@ -1,3 +1,19 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "../screens//home/HomeScreen";
+import PostScreen from "../screens/post/PostScreen";
+import ProfileScreen from "../screens/profile/ProfileScreen";
+import TabType from "./TabType";
 
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator<TabType>();
+
+function MainTabNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen}></Tab.Screen>
+      <Tab.Screen name="Post" component={PostScreen}></Tab.Screen>
+      <Tab.Screen name="Profile" component={ProfileScreen}></Tab.Screen>
+    </Tab.Navigator>
+  );
+}
+
+export default MainTabNavigator;
