66a95411ba65f37f30e0ed728caae986362a9748
Seiya <seiya314159265@icloud.com>
2026-09-02 00:07:21 +0900

    fix:footerのcomponentの中身を削除しました。

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,12 +1,5 @@
-import { NavigationContainer } from "@react-navigation/native";
-
 function FooterComponent() {
-  return (
-    <NavigationContainer
-      onStateChange={(state) => console.log(`New state is`, state)}
-    >
-      {" "}
-      {/* ここ文字なんと書けます！ */}
-    </NavigationContainer>
-  );
+  return null;
 }
+
+export default FooterComponent;
