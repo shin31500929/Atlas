323c3692f251ca4f91dd52949e368414aa46f853
shin <xxin11873@gmail.com>
2026-09-13 02:00:15 +0900

    NestJS,supabaseの接続

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecordsModule } from './records/records.module';
 import { PostsModule } from './posts/posts.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [RecordsModule, PostsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    RecordsModule,
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
